b417a56e12b3ce393758d273ea958564f3312b0a
raman41k <romangugel@gmail.com>
2023-12-17 15:58:20 +0200

    Make fibonacci function

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo 'hello';
+function fibonacci(int $amount)
+{
+    $fibonacci = [0, 1];
+    if ($amount <= 2) return $fibonacci;
+
+    for ($i = 2; $i < $amount; $i++) {
+        $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
+    }
+
+    return $fibonacci;
+}
+
+echo '<pre>';
+print_r(fibonacci(10));
+echo '</pre>';
