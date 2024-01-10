ee68d57bdbff532b66418b36ae6a157b74a8f659
roman <Huhel.Roman@leoceramika.com>
2024-01-10 11:24:50 +0200

    start hw8

--- index.php
+++ index.php
@@ -1,2 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Homework 8</title>
+</head>
+<body>
 <?php
-echo 'Hello';
+    session_start();
+   
+    if (isset($_POST["username"]) && isset($_POST["password"])){
+        $_SESSION['users'][] = [
+            'username' => $_POST['username'],
+            'password' => $_POST['password'],
+        ];  
+        $id = count($_SESSION['users']) - 1;
+    }
+   
+    // echo "<pre>";
+    //     print_r($_SESSION['users']);
+    // echo "</pre>";
+?>
+<div class="min-h-screen flex items-center justify-center bg-gray-200">
+    <div class="w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" action="">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                </label>
+                <input required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" name="username">
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                       id="password" type="password" placeholder="******************" name="password">
+            </div>
+            <div class="flex items-center justify-center">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Sign In
+                </button>
+            </div>
+            <?php if($_SESSION['users'][$id]) : ?>
+                <div class="bg-green-200 text-green-800 p-4 mt-3 rounded-md shadow-md text-xs text-center">
+                    Congratulations <?php echo $_SESSION['users'][$id]['username']?>! You successfully logged in
+                </div>
+            <?php endif; ?>
+        </form>
+    </div>
+</div>
+</body>
+</html>
